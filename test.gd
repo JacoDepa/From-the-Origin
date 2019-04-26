cfa8b9ef5415464e1429678e2d374e38f8ac2aaf
JacoDepa <jacopo.depasquale@gmail.com>
2019-04-26 20:01:36 +0200

    added food

--- test.gd
+++ test.gd
@@ -9,35 +9,75 @@ onready var nwood = 50
 onready var flag = 0
 onready var lumberjack = 0
 onready var stone_gatherer = 0
-onready var timer = null
+onready var farmer = 0
+onready var timerWood = null
+onready var timerStone = null
+onready var timerFood = null
 onready var delay = 10
-onready var can_gather = true
+onready var can_gather_wood = true
+onready var can_gather_stone = true
+onready var can_gather_food = true
 onready var lumberjack_plus = get_node("lumberjack+")
 onready var lumbrjack_min = get_node("lumberjack-")
+onready var nlumberjack = get_node("nlumberjack")
 onready var stone_gatherer_plus = get_node("stone_gatherer+")
 onready var stone_gatherer_min = get_node("stone_gatherer-")
+onready var nstone_gatherer = get_node("nstone_gatherer")
+onready var farmer_plus = get_node("farmer+")
+onready var farmer_min = get_node("farmer-")
+onready var nfarmer = get_node("nfarmer")
 
 func _ready():
-	timer = Timer.new()
-	timer.set_one_shot(true)
-	timer.set_wait_time(delay)
-	timer.connect("timeout", self, "on_timeout_complete")
-	add_child(timer)
+	timerWood = Timer.new()
+	timerWood.set_one_shot(true)
+	timerWood.set_wait_time(delay)
+	timerWood.connect("timeout", self, "on_timeout_complete_wood")
+	add_child(timerWood)
+	
+	timerStone = Timer.new()
+	timerStone.set_one_shot(true)
+	timerStone.set_wait_time(delay)
+	timerStone.connect("timeout", self, "on_timeout_complete_stone")
+	add_child(timerStone)
+	
+	timerFood = Timer.new()
+	timerFood.set_one_shot(true)
+	timerFood.set_wait_time(delay)
+	timerFood.connect("timeout", self, "on_timeout_complete_food")
+	add_child(timerFood)
+	
+	
 	lumberjack_plus.visible = false
 	lumbrjack_min.visible = false
+	nlumberjack.visible = false
 	stone_gatherer_plus.visible = false
 	stone_gatherer_min.visible = false
+	nstone_gatherer.visible = false
+	farmer_plus.visible = false
+	farmer_min.visible = false
+	nfarmer.visible = false
+	
+	
 	pass
 
 
-func on_timeout_complete():
-	can_gather = true
-	auto_gather()
+func on_timeout_complete_wood():
+	can_gather_wood = true
+	auto_gather_wood()
+
+func on_timeout_complete_stone():
+	can_gather_stone = true
+	auto_gather_stone()
+
+func on_timeout_complete_food():
+	can_gather_food = true
+	auto_gather_food()
 
 func _on_gather_wood_input_event(viewport, event, shape_idx):
 	if event is InputEvent:
 		if event.is_pressed():
 			wood += 1
+	
 	pass # Replace with function body.
 
 
@@ -45,6 +85,7 @@ func _on_gather_stone_input_event(viewport, event, shape_idx):
 	if event is InputEvent:
 		if event.is_pressed():
 			stone += 1
+	
 	pass # Replace with function body.
 
 
@@ -64,8 +105,14 @@ func _on_build_hut_input_event(viewport, event, shape_idx):
 	if hut > 0:
 		lumberjack_plus.visible = true
 		lumbrjack_min.visible = true
+		nlumberjack.visible = true
 		stone_gatherer_plus.visible = true
 		stone_gatherer_min.visible = true
+		nstone_gatherer.visible = true
+		farmer_plus.visible = true
+		farmer_min.visible = true
+		nfarmer.visible = true
+	
 	pass # Replace with function body.
 
 
@@ -73,6 +120,7 @@ func _on_gather_food_input_event(viewport, event, shape_idx):
 	if event is InputEvent:
 		if event.is_pressed():
 			food += 1
+	
 	pass # Replace with function body.
 
 
@@ -84,6 +132,7 @@ func _on_lumberjack_input_event(viewport, event, shape_idx):
 				pop -= 1
 				get_node("nlumberjack").text = str("lumberjack: ",lumberjack)
 				get_node("npopulation").text = str("population: ", pop)
+		auto_gather_wood()
 	pass # Replace with function body.
 
 
@@ -106,6 +155,7 @@ func _on_stone_gatherer_input_event(viewport, event, shape_idx):
 				pop -= 1
 				get_node("nstone_gatherer").text = str("stone gatherer: ",stone_gatherer)
 				get_node("npopulation").text = str("population: ", pop)
+		auto_gather_stone()
 	pass # Replace with function body.
 
 
@@ -119,30 +169,65 @@ func _on_stone_gatherer_min_input_event(viewport, event, shape_idx):
 				get_node("npopulation").text = str("population: ", pop)
 	pass # Replace with function body.
 
-func auto_gather():
+
+
+func _on_farmer_input_event(viewport, event, shape_idx):
+	if event is InputEvent:
+		if event.is_pressed():
+			if pop > 0:
+				farmer += 1
+				pop -= 1
+				get_node("nfarmer").text = str("farmer: ",farmer)
+				get_node("npopulation").text = str("population: ", pop)
+		auto_gather_food()
+	pass # Replace with function body.
+
+
+func _on_farmer_min_input_event(viewport, event, shape_idx):
+	if event is InputEvent:
+		if event.is_pressed():
+			if farmer > 0:
+				farmer -= 1
+				pop += 1
+				get_node("nfarmer").text = str("farmer: ",farmer)
+				get_node("npopulation").text = str("population: ", pop)
+	pass # Replace with function body.
+
+
+func auto_gather_wood():
 	if lumberjack >= 1:
-		#print(can_gather)
-		if can_gather == true:
-			#print("ok")
+		if can_gather_wood == true:
 			wood += (2*lumberjack)
-			can_gather = false
-			timer.start()
-			#print(timer.get_time_left())
-			get_node("/root/Node/nwood").text = str("wood: ", wood)
+			can_gather_wood = false
+			timerWood.start()
+			get_node("nwood").text = str("wood: ", wood)
 		else:
 			pass
 	else:
 		pass
 		
+
+func auto_gather_stone():
 	if stone_gatherer >= 1:
-		#print(can_gather)
-		if can_gather == true:
-			#print("ok")
+		if can_gather_stone == true:
 			stone += (2*stone_gatherer)
-			can_gather = false
-			timer.start()
-			#print(timer.get_time_left())
-			get_node("/root/Node/nstone").text = str("stone: ", stone)
+			can_gather_stone = false
+			timerStone.start()
+			get_node("nstone").text = str("stone: ", stone)
+		else:
+			pass
+	else:
+		pass
+		
+
+
+func auto_gather_food():
+	if farmer > 0:
+		if can_gather_food == true:
+			food += (2*farmer)
+			can_gather_food = false
+			timerFood.start()
+			get_node("nfood").text = str("food: ", food)
 		else:
 			pass
 	else:
--- wood.gd
+++ wood.gd
@@ -1,96 +1,18 @@
 extends Area2D
 
-onready var pop = 0
 onready var wood = 0
-onready var nwood = 50
-onready var flag = 0
-onready var lumberjack = 0
-onready var timer = null
-onready var delay = 10
-onready var can_gather = true
+
 
 
 func _ready():
-	timer = Timer.new()
-	timer.set_one_shot(true)
-	timer.set_wait_time(delay)
-	timer.connect("timeout", self, "on_timeout_complete")
-	add_child(timer)
-	#print("dentro func_ready")
-	#print(timer)
+	
 	pass
 
-func on_timeout_complete():
-	can_gather = true
-	auto_gather()
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
-	
 	if event is InputEvent:
 		if event.is_pressed():
 			wood += 1
 			get_node("/root/Node/nwood").text = str("wood: ",wood)
 	
-	
 	pass # Replace with function body.
-
-
-
-
-func _on_build_hut_input_event(viewport, event, shape_idx):
-	if wood >= nwood:
-		if event is InputEvent:
-			if event.is_pressed():
-				wood -= nwood
-				flag = 1
-				pop += 4
-				get_node("/root/Node/nwood").text = str("wood: ",wood)
-	if flag == 1:
-		nwood += 50
-		flag = 0
-	pass # Replace with function body.
-
-
-
-#func _on_lumberjackplus_input_event(viewport, event, shape_idx):
-	
-	#pass # Replace with function body.
-
-
-func _on_lumberjackminus_input_event(viewport, event, shape_idx):
-	if lumberjack >= 1:
-		if event is InputEvent:
-			if event.is_pressed():
-				lumberjack -= 1
-	pass # Replace with function body.
-
-
-	
-
-func _on_lumberjack_input_event(viewport, event, shape_idx):
-	#print("entro in func_lumberjack")
-	if event is InputEvent:
-		if event.is_pressed():
-			if pop >= 1:
-				lumberjack += 1
-				#print(lumberjack)
-				pop -= 1
-	#print("func_auto_gather")
-	auto_gather()
-	pass # Replace with function body.
-
-func auto_gather():
-	if lumberjack >= 1:
-		#print(can_gather)
-		if can_gather == true:
-			#print("ok")
-			wood += (2*lumberjack)
-			can_gather = false
-			timer.start()
-			#print(timer.get_time_left())
-			get_node("/root/Node/nwood").text = str("wood: ", wood)
-		else:
-			pass
-	else:
-		pass
-	
